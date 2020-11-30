46a3da65fd14f66bfdaf770d101517c1bf88c2e2
Jacek Bogocz <jacek.bogocz@gmail.com>
2020-11-30 03:24:12 +0100

    train model and save results to blobs folder

--- run.py
+++ run.py
@@ -11,3 +11,6 @@ seed = 42
 argv = sys.argv[1:]
 abs_path = os.path.dirname(os.path.abspath(__file__))
 path = os.path.join(abs_path, 'data', argv[0]) # blogtext.csv.zip
+
+# Run training & save best model to the blobs folder
+run_train_model(abs_path, path, seed)
