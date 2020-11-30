01dc01fa91c6d66d9700fa186413488bb30f9ec3
Jacek Bogocz <jacek.bogocz@gmail.com>
2020-11-30 03:23:40 +0100

    get args from command line

--- run.py
+++ run.py
@@ -3,3 +3,11 @@ import os
 import sys
 # Train model
 from train.model_train import run_train_model
+
+# Seed
+seed = 42
+
+# Get the arguments from the command-line
+argv = sys.argv[1:]
+abs_path = os.path.dirname(os.path.abspath(__file__))
+path = os.path.join(abs_path, 'data', argv[0]) # blogtext.csv.zip
